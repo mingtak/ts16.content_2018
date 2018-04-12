b95abc3a9903552678183242e6c1b32129bb698a
Andy Fang <andy@mingtak.com.tw>
2018-04-12 19:52:25 +0800

    update event handler

--- src/ts16/content/event/event.py
+++ src/ts16/content/event/event.py
@@ -3,8 +3,17 @@ from plone import api
 from ts16.content import _
 
 
+def toFolderContents(object, event):
+    try:
+        portal = api.portal.get()
+        lang = api.portal.get_current_language()
+        request = portal.REQUEST
+        request.response.redirect('%s/%s/folder_contents' % (portal.absolute_url(), lang))
+    except:
+        pass
+
+
 def userLogin(event):
     portal = api.portal.get()
     request = portal.REQUEST
     request.response.redirect('%s/folder_contents' % portal.absolute_url())
-
