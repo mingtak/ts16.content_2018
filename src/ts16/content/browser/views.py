da41ab0788838a269632f781e4c8c6f9a4df6e5e
Andy Fang <andy@mingtak.com.tw>
2018-04-12 15:50:32 +0800

    update update 0412, empty

--- src/ts16/content/browser/views.py
+++ src/ts16/content/browser/views.py
@@ -6,6 +6,26 @@ from plone import api
 
 class CoverView(BrowserView):
     template = ViewPageTemplateFile('template/cover_view.pt')
-    
+
+    def getNews(self):
+        return api.content.find(context=self.portal[self.lang]['news'], Type='News Item')
+
+    def getAward(self):
+        return api.content.find(context=self.portal[self.lang]['award'], Type='Page')
+
+    def getDocumentShow(self):
+        return api.content.find(context=self.portal[self.lang]['document_show'], Type='Page')
+
+    def getMeetingRoom(self):
+        return api.content.find(context=self.portal[self.lang]['meeting_room'], Type='Page')
+
+    def getAbout(self):
+        return api.content.find(context=self.portal[self.lang]['about'], Type='Page')
+
+    def getForThis(self):
+        return api.content.find(context=self.portal[self.lang]['for_this'], Type='Page')
+
     def __call__(self):
+        self.portal = api.portal.get()
+        self.lang = api.portal.get_current_language()
         return self.template()
